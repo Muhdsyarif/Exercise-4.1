62044dd7a8c477ecc8b8525758edd6edb4a1f0bb
Muhdsyarif <72724907+Muhdsyarif@users.noreply.github.com>
2021-04-25 06:19:10 +0800

    Add files via upload

---
+++ src/Canon.java
@@ -0,0 +1,80 @@
+import java.util.Scanner;
+public class Canon extends Printer {									
+
+Scanner s = new Scanner(System.in);
+	
+	private String brand, type, colour, onoff;
+	private double input_tray, speed, price, quantity, total_price;
+	
+	
+	Canon(){
+		this.brand = "------Canon Pixma------";
+		System.out.print("Enter type of printer(Inkjet/Laser): ");
+		this.type = s.nextLine();
+		System.out.print("Enter the colour: ");
+		this.colour = s.nextLine();
+		System.out.print("Enter total input tray: ");
+		this.input_tray = s.nextDouble();
+		System.out.print("Printing speed: ");
+		this.speed = s.nextDouble();
+		System.out.print("Enter the printer price: ");
+		this.price = s.nextDouble();
+		System.out.print("Enter the quantity: ");
+		this.quantity = s.nextDouble();
+		this.total_price = this.price * this.quantity; 			
+	}
+	
+	String onoff() {
+		System.out.print("Turn ON/OFF");
+		this.onoff = s.nextLine();
+		switch(onoff) {
+		
+			case "ON":
+				System.out.println("Printer is ready to use");
+				break;
+			
+			case "OFF":
+				System.out.println("Printer was shut down");
+				break;
+		}
+		return onoff;
+	}
+	
+	
+	//Getter Method
+	public String getBrand() {
+		return this.brand;
+	}
+	
+	public String getType() {
+		return this.type;
+	}
+	
+	public String getColour() {
+		return this.colour;
+	}
+	
+	public double getInputTray() {
+		return this.input_tray;
+	}
+	
+	public double getSpeed() {
+		return this.speed;
+	}
+	
+	public double getPrice() {
+		return this.price;
+	}
+	
+	public double getQuantity() {
+		return this.quantity;
+	}
+	
+	public double getTotalPrice() {
+		return this.total_price;
+	}
+	
+	public String getOnOff() {
+		return this.onoff;
+	}
+}
---
+++ src/Printer.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Printer {
+	
+	Scanner s = new Scanner(System.in);
+	
+	private double width, height, weight;
+	
+	
+	Printer(){
+		System.out.print("Please enter the width of printer: ");
+		this.width = s.nextDouble();
+		System.out.print("Please enter the height of printer: ");
+		this.height = s.nextDouble();
+		System.out.print("Please enter the weight of printer: ");
+		this.weight = s.nextDouble();
+	}
+	
+	double getWidth() {
+		return this.width;
+	}
+	
+	double getHeight() {
+		return this.height;
+	}
+	
+	double getWeight() {
+		return this.weight;
+	}
+}
